8321209c3a6ded349c11fa29a7165f8c45f580d3
igorpuszman <igibabes@gmail.com>
2020-04-24 08:32:29 +0200

    zadanie

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	   string imie="Igor";
+	   string nazwisko="Puszman";
+	  
+	   
+    ofstream wyjscie;
+    wyjscie.open("out.json");
+
+plik << " [ \n { \n \"imie\" : \" " << imie;
+plik << " [ \n { \n \"nazwisko\" : \ " " << nazwisko;
+plik.close();
+
+cout<<endl;
+system("PAUSE")
+	return 0;
+}
